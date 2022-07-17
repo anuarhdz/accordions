ea1aa93bca6e9475513d6376ddb87165d4c71ef1
anuarhdz <anuar@hey.com>
2022-07-17 04:20:57 -0500

    add accordion base behavior

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 class Accordions {
   constructor(element) {
     this.accordionsContainer = element;
-    this.accordionsPanels = this.accordionsContainer.querySelectorAll("h2");
+    this.accordionControls = Array.from(this.accordionsContainer.querySelectorAll(".accordion-control"));
+    this.accordionPanels = this.accordionsContainer.querySelectorAll(".accordion-panel");
+    this.currentControlClicked = {};
+    this.controlsReady = false;
+    this.panelsReady = false;
+    this.clickEventHandler = this.clickEvent.bind(this);
+
+    this.init();
   }
 
   init() {
-    return this.accordionsPanels;
+    this.setControls();
+    this.setPanels();
+    this.connectComponents();
   }
 
-  setPanels() {
-    this.accordionsPanels.forEach(function(panel){
-      panel.firstElementChild.setAttribute("aria-expanded", false);
+  setControls() {
+    this.accordionControls.forEach(function (panel) {
+      panel.setAttribute("aria-expanded", false);
     });
+    this.controlsReady = true;
+  }
+  setPanels() {
+    this.accordionPanels.forEach(function (panel) {
+      panel.setAttribute("aria-hidden", true);
+      panel.setAttribute("role", "region");
+    });
+    this.panelsReady = true;
+  }
+  connectComponents() {
+    for (let control of this.accordionControls) {
+      control.setAttribute("aria-controls", control.parentElement.nextElementSibling.id);
+      control.addEventListener("click", this.clickEventHandler);
+    }
+
+    for (let panel of this.accordionPanels) {
+      panel.setAttribute("aria-labelledby", panel.previousElementSibling.firstElementChild.id);
+    }
+  }
+
+  clickEvent(e) {
+    this.currentControlClicked = {
+      control: e.target,
+      panel: e.target.parentElement.nextElementSibling,
+      status: e.target.getAttribute("aria-expanded") === "true" ? "opened" : "closed",
+    };
+
+    if (this.currentControlClicked.status === "closed") {
+      this.openPanel(this.currentControlClicked);
+    } else if (this.currentControlClicked.status === "opened") {
+      this.closePanel(this.currentControlClicked);
+    }
+  }
+
+  openPanel(component) {
+    component.panel.removeAttribute("aria-hidden");
+    component.panel.classList.add("growth");
+    component.panel.classList.remove("shrink");
+    component.control.setAttribute("aria-expanded", true);
+    component.status = "opened";
+  }
+
+  closePanel(component) {
+
+    component.panel.classList.remove("growth");
+    component.panel.classList.add("shrink");
+    // this.delay = setTimeout(function () {
+    //   component.panel.setAttribute("aria-hidden", true);
+    // }, 40);
+    component.control.setAttribute("aria-expanded", false);
+    component.status = "closed";
+    component.panel.setAttribute("aria-hidden", true);
   }
 }
 
